14eeea572a8c8005231968a63145a56b011b6f18
PauloBrombley <paulo.brombley@gmail.com>
2023-03-06 15:49:22 -0300

    First commit

---
+++ src/main/java/com/example/visualnuts/VisualNutsApplication.java
@@ -0,0 +1,13 @@
+package com.example.visualnuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualNutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualNutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/visualnuts/VisualNutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visualnuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualNutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
